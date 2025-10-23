9c1616f2c1c27c5d2f98ded4410d259b919c08b7
najmabashiir <najaxbashiirali>
2025-10-23 11:23:49 +0300

    Create Asset folder
    Signed-off-by: najmabashiir <najaxbashiirali>

--- lib/main.dart
+++ lib/main.dart
@@ -1,26 +1,36 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MaterialApp(
-    home: Scaffold(
+  runApp(
+    const MaterialApp(
+      home: MyApp(),
+      debugShowCheckedModeBanner: false,
+    ),
+  );
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
       backgroundColor: Colors.blueGrey[200],
       appBar: AppBar(
-        title: Text(
-          "hello said",
+        title: const Text(
+          "Hello Said",
           style: TextStyle(color: Colors.white),
         ),
         backgroundColor: Colors.blueGrey[900],
       ),
-      body: Center(
-        child:Image(
-          image:
-          AssetImage('images/diamond.jpg')
-          ),
+      body:  Center(
+        child: Image(
+          image: AssetImage('images/pizz1.jpg'),
+        ),
       ),
-    ),
-  )
-  );
+    );
+  }
 }
-    
+
 
 
